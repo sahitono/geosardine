917e96f025aa1b18afa104ce6291455ca1c42db0
Sahit Tuntas Sadono <26474008+sahitono@users.noreply.github.com>
2020-12-05 15:02:04 +0700

    Update test_geosardine_interpolate.py
    black reformat

--- tests/test_geosardine_interpolate.py
+++ tests/test_geosardine_interpolate.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-
 from geosardine import interpolate
 from geosardine._utility import calc_extent
 
@@ -31,13 +30,16 @@ def test_idw():
 
     assert interpolate.idw_single([101, -7], xy, values) == 113.8992997794633
     assert (
-        round(interpolate.idw_single(
-            [860209, 9295740],
-            np.array([[767984, 9261620], [838926, 9234594]]),
-            np.array([[101.1, 102.2]]),
-            epsg=32748,
-        ),8)
-        == round(101.86735169471324,8)
+        round(
+            interpolate.idw_single(
+                [860209, 9295740],
+                np.array([[767984, 9261620], [838926, 9234594]]),
+                np.array([[101.1, 102.2]]),
+                epsg=32748,
+            ),
+            8,
+        )
+        == round(101.86735169471324, 8)
     )
 
     with pytest.raises(ValueError):
